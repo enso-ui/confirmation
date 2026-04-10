fec674942709bbae408c6af514eaa53f6a1a11ca
mishu <mishu@xtelecom.ro>
2026-04-10 12:26:38 +0300

    ready for enso v9

--- src/bulma/Confirmation.vue
+++ src/bulma/Confirmation.vue
@@ -7,7 +7,7 @@
             <a class="button is-small is-success mr-2"
                 @click="confirm">
                 <span class="icon is-small">
-                    <fa icon="check"/>
+                    <fa :icon="faCheck"/>
                 </span>
             </a>
         </template>
@@ -15,7 +15,7 @@
             <a class="button is-small is-danger"
                 @click="cancel">
                 <span class="icon is-small">
-                    <fa icon="times"/>
+                    <fa :icon="faXmark"/>
                 </span>
             </a>
         </template>
@@ -24,15 +24,17 @@
 
 <script>
 import { FontAwesomeIcon as Fa } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import CoreConfirmation from '../components/CoreConfirmation.vue';
-
-library.add(faCheck, faTimes);
 
 export default {
     name: 'Confirmation',
 
     components: { Fa, CoreConfirmation },
+
+    data: () => ({
+        faCheck,
+        faXmark,
+    }),
 };
 </script>
